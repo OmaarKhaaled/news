ce23b8844d84b2f44ac1e9d59d208752c48dc648
omar khaled <omarkhaled10102004@gmail.com>
2025-08-15 15:13:10 +0300

    Initialize project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:news/home_screen.dart';
+
+void main() {
+  runApp( NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName :(_) =>HomeScreen() ,
+      },
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
